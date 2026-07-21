c98a02fc4f9f8496c74a73f6219da215dbe54aeb
Abhijeet Gawali <gawalishambu@gmail.com>
2026-07-21 14:58:40 +0530

    algoritham and logic for api responce

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,16 @@ import asyncHandler from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler(async (req, res) => {
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // upload them to cloudinary , avatar
+    // create user objet - create emtry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return responce
+
+    
     res.status(200).json({
         message: "ok"
     })
