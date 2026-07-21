e74d21ed4ddffa7866cc4d16348d312f9afee168
Abhijeet Gawali <gawalishambu@gmail.com>
2026-07-21 14:38:04 +0530

    all setup complete

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dns from "dns"
 
+// Change DNS
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
 const connectDB = async () => {
     try {
