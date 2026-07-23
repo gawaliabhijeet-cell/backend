97c9623e3c0257d490ed10d1f94b5542cc156222
Abhijeet Gawali <gawalishambu@gmail.com>
2026-07-23 17:58:21 +0530

    add login logout , auth.middlerware, also apdate

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,18 @@ import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponce.js";
 import { upload } from "../middlewares/multer.js";
 
+const generateAccessAndRefershTokens = async (userId) => {
+  const user = await User.findById(userId);
+  const accessToken = user.generateAccessToken();
+  const refrechToken = user.generateRefreshToken();
+
+  // user bar bar login na kearn padhe
+  user.refrechToken = refrechToken;
+  await user.save({ validateBeforeSave: false });
+
+  return { accessToken, refrechToken };
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
   // validation - not empty
@@ -81,4 +93,65 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User register sucessesfully"));
 });
 
-export default registerUser;
+const loginUser = asyncHandler(async (req, res ) => {
+  // req body => data
+  // username or email
+  // find the user
+  // password check
+  // access and referesh token
+  // send cookie
+
+  const {email, ucookiesername, password } = req.body
+
+  if(!username || !email) {
+    throw new ApiError(400 , "username or emaail is required")
+
+  }
+
+    const user = await User.findOne({
+      $or : [{username} , {email}]
+    })
+
+    if(!user) {
+      throw new ApiError(404, "User does not exit")
+    }
+
+   const isPasswordValid = await user.isPasswordCorrect(password)
+
+   if(!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
+   }
+
+   const {accessToken , refrechToken } = await 
+   generateAccessAndRefershTokens(user._id)
+
+   const loggedInUser = await User.findById(user._id).
+   select("-password -refreshToken")
+
+   // cokies
+   const options = {
+      httpOnly : true,
+      secure: true
+   }
+
+   return res.status(200).cookie("accessToken", accessToken, options)
+   .cookie("refreshToken" , refrechToken ,options)
+   .json(
+    new ApiResponse(
+      200, {
+        user: loggedInUser, accessToken, refrechToken
+      },
+      "User logged in successfully"
+    )
+   )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+})
+
+ 
+export  {registerUser,
+  loginUser,
+  logoutUser
+};
