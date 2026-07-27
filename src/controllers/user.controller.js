6eaa23e851c7843b02169502e206031fe4540daa
Abhijeet Gawali <gawalishambu@gmail.com>
2026-07-27 16:15:46 +0530

    add getCurrentUser and updateAccountDetails endpoints for user profile management

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,7 +225,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   
   const {oldPassword, newPassword} = req.body
 
-  const user = await User.findById(req.user?.id)
+  const user = await User.findById(req.user?._id)
    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
    if(!isPasswordCorrect){
@@ -240,9 +240,42 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
    .json(new ApiResponse(200 , {}, "password is correct"))
 })
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+  return res
+  .status(200)
+  .json(new ApiResponse(200, req.user, "current user fetched successfully"))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+  const {fullName, email} = req.body
+
+  if(!fullName || !email) {
+    throw new ApiError(400, "All fields are required")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email
+      }
+    },
+    {new: true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, "account updated successfully"))
+})
+
+
 
 export  {registerUser,
   loginUser,
   logoutUser,
-  refreshAccessToken 
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails
 };
